53de1332addfb815ebb6460340464f8fa9193dcd
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-05-10 16:50:09 +0300

    Update-2 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,19 +9,6 @@ const refs = {
 
 refs.form.addEventListener('submit', onPromiseCreate);
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
-
 function onPromiseCreate(e) {
   e.preventDefault();
 
@@ -44,4 +31,17 @@ function onPromiseCreate(e) {
       });
     delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
